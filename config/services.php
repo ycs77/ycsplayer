84ecc24172c560ee5d663ae4cff496ec61348986
Lucas Yang <yangchenshin77@gmail.com>
2023-08-31 02:43:17 +0800

    Support mailgun driver

--- config/services.php
+++ config/services.php
@@ -15,6 +15,7 @@ return [
     */
 
     'mailgun' => [
+        'transport' => 'mailgun',
         'domain' => env('MAILGUN_DOMAIN'),
         'secret' => env('MAILGUN_SECRET'),
         'endpoint' => env('MAILGUN_ENDPOINT', 'api.mailgun.net'),
