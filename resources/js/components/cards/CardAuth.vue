1580e63bc4ab510e7023895fa89736289d58d043
Lucas Yang <yangchenshin77@gmail.com>
2023-08-31 16:21:26 +0800

    Fix mobile card no full width

--- resources/js/components/cards/CardAuth.vue
+++ resources/js/components/cards/CardAuth.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="max-w-sm mx-auto px-[--layout-gap] pb-[--layout-gap] lg:px-[--layout-gap-lg] lg:pb-[--layout-gap-lg]">
+  <div class="mx-auto px-[--layout-gap] pb-[--layout-gap] sm:max-w-sm lg:px-[--layout-gap-lg] lg:pb-[--layout-gap-lg]">
     <div class="bg-blue-950/50 p-4 rounded-lg lg:p-6">
       <h1 class="text-2xl">{{ title }}</h1>
 
