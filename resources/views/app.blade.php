a9583c046fe48c8cb9a3f27c78676a8424b597a4
Lucas Yang <yangchenshin77@gmail.com>
2023-09-10 16:19:38 +0800

    Add ogimage

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,6 +6,12 @@
 
     @inertiaHead
 
+    <meta name="description" content="線上影音點播包廂">
+    <meta property="og:title" content="ycsPlayer">
+    <meta property="og:description" content="線上影音點播包廂">
+    <meta property="og:image" content="{{ config('app.url') }}/og-20230910.png">
+    <meta name="twitter:card" content="summary_large_image">
+
     @vite('resources/js/app.ts')
 </head>
 
--- resources/views/landing-page.blade.php
+++ resources/views/landing-page.blade.php
@@ -5,9 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>ycsPlayer</title>
+
+    <meta name="description" content="線上影音點播包廂">
     <meta property="og:title" content="ycsPlayer">
     <meta property="og:description" content="線上影音點播包廂">
-    <meta name="description" content="線上影音點播包廂">
+    <meta property="og:image" content="{{ config('app.url') }}/og-20230910.png">
+    <meta name="twitter:card" content="summary_large_image">
 
     @vite('resources/css/landing-page.css')
 </head>
