64b5155219f5a7f08e9bbcba71c44200252ce259
Lucas Yang <yangchenshin77@gmail.com>
2023-09-13 16:59:49 +0800

    Move forgt password status message to top

--- resources/js/pages/Auth/ForgotPassword.vue
+++ resources/js/pages/Auth/ForgotPassword.vue
@@ -1,15 +1,15 @@
 <template>
   <CardAuth title="忘記密碼">
     <form @submit.prevent="form.post('/forgot-password')">
-      <div class="space-y-6">
-        <TextInput id="email" v-model="form.email" label="E-mail" type="email" />
-      </div>
-
       <div
         v-if="status"
-        class="mt-4 bg-blue-950/75 px-3 py-2 text-sm text-blue-400 border border-blue-400 rounded-md"
+        class="mb-4 bg-blue-950/75 px-3 py-2 text-sm text-blue-400 border border-blue-400 rounded-md"
       >
         {{ status }}
+      </div>
+
+      <div class="space-y-6">
+        <TextInput id="email" v-model="form.email" label="E-mail" type="email" />
       </div>
 
       <div class="mt-6">
