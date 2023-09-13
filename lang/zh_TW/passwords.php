d0923b81f99c7f0fce548ead4ae1ef43a7a4fbfa
Lucas Yang <yangchenshin77@gmail.com>
2023-09-13 16:59:24 +0800

    Tweak

--- lang/zh_TW/passwords.php
+++ lang/zh_TW/passwords.php
@@ -16,5 +16,5 @@ return [
     'sent'      => '密碼重設郵件已發送！',
     'throttled' => '請稍候再試。',
     'token'     => '密碼重設碼無效。',
-    'user'      => '找不到該 E-mail 對應的使用者。',
+    'user'      => 'E-mail 未註冊。',
 ];
