4ddba128bf5cfa56e4a008313e949ab21a24416f
Lucas Yang <yangchenshin77@gmail.com>
2023-11-03 23:27:34 +0800

    Update PusherWebhookReceived's public properties into construct

--- app/Broadcasting/Events/PusherWebhookReceived.php
+++ app/Broadcasting/Events/PusherWebhookReceived.php
@@ -9,13 +9,10 @@ class PusherWebhookReceived
 {
     use Dispatchable, SerializesModels;
 
-    public int $time_ms;
-
-    public array $events;
-
-    public function __construct(int $time_ms, array $events)
-    {
-        $this->time_ms = $time_ms;
-        $this->events = $events;
+    public function __construct(
+        public int $time_ms,
+        public array $events,
+    ) {
+        //
     }
 }
