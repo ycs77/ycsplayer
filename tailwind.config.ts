94fca2644f1d43a41d711d238c816b2e25ad37c9
Lucas Yang <yangchenshin77@gmail.com>
2024-09-03 14:44:07 +0800

    Bump dependencies

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,10 +11,10 @@ export default {
   ],
   theme: {
     screens: {
-      'sm': '640px',
-      'md': '768px',
-      'lg': '1024px',
-      'xl': '1280px',
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
       '2xl': '1536px',
     },
     extend: {
